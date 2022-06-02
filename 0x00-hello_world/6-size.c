d235bb1fee88f90c90b7500179bfee297febf98f
Ibrahim Mwakaba <imwakaba@gmail.com>
2022-06-02 11:51:46 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
-
 /**
  * main - Entry point to the program
  * Description :print sizes of the data type
  * Return::Always 0 (Success)
  */
-
 int main(void)
 {
 	char a;
@@ -19,6 +17,5 @@ int main(void)
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 	printf("Size of a float: %zu byte(s)\n", sizeof(e));
-
-	return (0);
+        return (0);
 }
