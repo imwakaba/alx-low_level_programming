72209313f6dcd1903b096ed4fd44fea486c148d3
Ibrahim Mwakaba <imwakaba@gmail.com>
2022-06-03 13:41:52 -0700

    commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,12 +11,12 @@
 
 int main(void)
 {
-	char low;
+	char c;
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		putchar('\n');
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
 
-	return (0);
+return (0);
 }
 
