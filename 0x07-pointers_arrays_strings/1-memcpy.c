729264f38499012e1599446910383111a25afdee
Ibrahim Mwakaba <imwakaba@gmail.com>
2022-06-20 07:44:08 -0700

    pointers and arrays

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,15 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
-#include "main.h"
 
 /**
  * _memcpy - copies n bytes from area src to memory area dest
