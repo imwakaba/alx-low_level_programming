5e71979e225226c95e09edca0d54a7579008f0a0
Ibrahim Mwakaba <imwakaba@gmail.com>
2022-07-06 08:06:24 -0700

    Fuction pointer

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,13 @@
-#include <stdio.h>
-#include "function_pointers.h"
-
 /**
- * print_name - prints a name
- * @name: string containing the name
- * @f: pointer to function
+ * print_name - function with two arguments
+ * @name: char type pointer to string
+ * @f: pointer to a function
  *
+ * Description: prints a name using function pointers
+ * Return: void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-	{
-		(f) (name);
-	}
+	if (name && f)
+		(*f)(name);
 }
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -1,1 +1,8 @@
-0x0F. C - Function pointers
+0x0E. C - Function pointers
+Description
+This project in the Low Level Programming series is about:
+
+What are function pointers and how to use them
+What does a function pointer exactly hold
+Where does a function pointer point to in the virtual memory
+Files
