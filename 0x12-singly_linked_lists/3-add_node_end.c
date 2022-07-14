13594fa73f3c9eb26932bdae4188979521a309bc
Ibrahim Mwakaba <imwakaba@gmail.com>
2022-07-14 04:35:07 -0700

    string single linked link

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,41 +1,35 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
- * *add_node_end - adds a new node at the end of a list_t list.
- * @head: node head.
- * @str: string.
- * Return: address of the new element.
+ * add_node_end - add a new node to end of linked list
+ * @head: double pointer reference head
+ * @str: string to add to new struct
+ *
+ * Return: address of new structure element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-unsigned int i;
-list_t *c;
-list_t *newnd;
+	size_t i;
+	list_t *new = malloc(sizeof(list_t));
+	list_t *last = *head;
 
-newnd = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = strdup(str);
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	new->len = i;
+	new->next = NULL;
 
-if (newnd ==  '\0')
-return (NULL);
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
 
+	while (last->next != NULL)
+		last = last->next;
+	last->next = new;
 
-for  (i = 0; str[i] != '\0'; i++)
-;
-
-newnd->str  = strdup(str);
-newnd->len  = i;
-
-if (*head == NULL)
-{
-*head = newnd;
-newnd->next = NULL;
-return (*head);
-}
-c = *head;
-while (c->next)
-{
-c = c->next;
-}
-c->next = newnd;
-newnd->next = NULL;
-return (*head);
+	return (new);
 }
