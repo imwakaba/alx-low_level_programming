743257d28ffeea4aaafdf3fdae34fa6457d3e1f5
Ibrahim Mwakaba <imwakaba@gmail.com>
2022-07-19 03:58:34 -0700

    more_singly_linked_lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,29 +1,26 @@
 #include "lists.h"
-
 /**
- * print_listint_safe - function that prints a linked list
- * @head: pointer to beginning of linked list linsint_t
- *
- * Return: number of nodes in the list or 98 if failure
- */
-
+* print_listint_safe - prints the circular linked list
+* @head: head
+* Return: return
+*/
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodeCount = 0;
+size_t a;
+listint_t *b = (listint_t *)head;
 
-	while (head && head > head->next)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		nodeCount++;
-		head = head->next;
-	}
-
-	if (head)
-	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-		nodeCount++;
-	}
-
-	return (nodeCount);
+a = 0;
+while (b && b > b->next)
+{
+printf("[%p] %d\n", (void *)b, b->n);
+b = b->next;
+a++;
+}
+if (b)
+{
+printf("[%p] %d\n", (void *)b, b->n);
+printf("-> [%p] %d\n", (void *)b->next, b->next->n);
+a++;
+}
+return (a);
 }
